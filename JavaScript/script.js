5418f2026e0fb632774ca78ec21b82afea3b3ffa
AuliaTzy友 <98168734+maspreset@users.noreply.github.com>
2022-11-13 17:46:43 +0700

    script.js

---
+++ JavaScript/script.js
@@ -0,0 +1,21 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+  strings: ["an Engineer", "Front-End Developer", "graphic design", "woman", "Blogger"],
+  typeSpeed: 90,
+  backSpeed: 40,
+  cursorChar: " |",
+  smartBackspace: true,
+  loop: true,
+});
+
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+  document.body.classList.toggle("dark-theme");
+  
+  if(document.body.classList.contains("dark-theme")) {
+    icon.src = "Images/sun.png";
+  } else {
+    icon.src = "Images/moon.png";
+  }
+}
